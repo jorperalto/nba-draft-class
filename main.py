cdf36a5692381921f2fe892a4e4aea818024648b
gioperalto <gyoperalto@gmail.com>
2020-06-25 18:11:10 -0400

    Added more player data

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 if __name__ == "__main__":
-    df, stats = pd.read_csv('players.csv'), ['PPG', 'RPG', 'APG', 'Position']
-    stat = int(input('Which stat does your team need?\n1 - PPG\n2 - RPG\n3 - APG\n4 - Position\nYour answer was: '))
+    stats = [' NAME', ' AGE', ' SALARY', ' PPG_LAST_SEASON', ' APG_LAST_SEASON', ' RPG_LAST_SEASON', ' POSITION']
+    df = pd.read_csv('players.csv').filter(items=stats)
+    stat = int(input(
+        'Which stat does your team need?\n' +
+        '1 - AGE\n2 - SALARY\n3 - PPG_LAST_SEASON\n4 - APG_LAST_SEASON\n5 - RPG_LAST_SEASON\n6 - POSITION' +
+        '\nYour answer was: '))
 
-    players_sorted = df.sort_values(stats[stat-1], ascending=False)
-    print('\n{} by player:\n {}\n'.format(stats[stat-1], players_sorted))
+    players_sorted = df.sort_values(stats[stat], ascending=False)
+    print('\n{} by player:\n {}\n'.format(stats[stat], players_sorted))
 
-    print('Top player by {}:\n{}'.format(stats[stat-1], players_sorted.iloc[0]))
+    print('Top player by {}:\n{}'.format(stats[stat], players_sorted.iloc[0]))
